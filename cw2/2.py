c8de72f6f842c73c381426d941240dc52ff5264a
BialoskorskiA <artur.bialoskorski@capgemini.com>
2019-10-14 13:47:18 +0200

    ssrednik zamiast kropwki w 2 zadaniu

--- cw2/2.py
+++ cw2/2.py
@@ -1,7 +1,7 @@
 import re
 
-file2 = 'ssa165161dafaf,5464,65464'
-a = re.match('^(\w)+[,][0-9]+[,][0-9]+$', file2)
+file2 = 'ssa165161dafaf;5464;65464'
+a = re.match('^(\w)+[;][0-9]+[;][0-9]+$', file2)
 
 if a:
     print('dopasowano')
